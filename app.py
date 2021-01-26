35691ed4431e498371f967e4281e6bd061059402
EsmaeiliSina <es.sina76@gmail.com>
2021-01-26 12:40:50 +0330

    update app.py file

--- app.py
+++ app.py
@@ -13,16 +13,17 @@ from instagrapi import Client
 cl = Client(json.load(open('settings.json')))
 print('Login Successfully.')
 
-media = cl.hashtag_medias_recent('python', amount=20) 
+media = cl.hashtag_medias_recent('python', amount=10) # get 10 recent post with python hashtag
 
 c = 0
 for m in media:
     c += 1
     try:
-        cl.media_like(m.id)
-        cl.media_comment(m.id , '😍😍')
+        cl.media_like(m.id)  
+        cl.media_comment(m.id , 'comment_text') # change comment text
         print(str(c) + ': ' + m.code + '\t\tTime: ' + str(m.taken_at) + '\t\tLike: ' + str(m.like_count) + '\t\tComment: ' + str(m.comment_count))
-        time.sleep(random.randint(40,60))
+        time.sleep(random.randint(30,60))       # sleep to avoid the account ban
     except Exception as e:
         print(e.args)
+
 print('Done')
